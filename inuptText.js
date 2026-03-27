340ff5c67ef97323f0565a6a085c81fb2c99302f
Tetsuya Hirotsu Dev <181657011+Tetsuya-Hirotsu-Dev@users.noreply.github.com>
2026-03-27 15:51:50 +0900

    20260327 1554

--- inuptText.js
+++ inuptText.js
@@ -2,6 +2,59 @@
 
 import * as utl from "./utils.js";
 
+/**
+ * @callback CallbackFormat
+ * @param {string} validValue
+ * @returns {string} formattedValue
+ */
+/**
+ * @callback CallbackCheck
+ * @param {string} value
+ * @param {string} lastFormattedValue
+ * @returns {{validValue: string} | {errorMessage: string, correctFormattedValue: string}} result
+ */
+/**
+ * @callback CallbackMultipleCheck
+ * @param {string} senderValue
+ * @param {string} senderLastFormattedValue
+ * @param {TextBox} SenderTextBox
+ * @returns {Promise<{validValue: string} | {errorMessage: string, textBoxes: TextBox[], correctFormattedValues: string[]}>} result
+ */
+
+/**
+ * @param {string} errorTitle
+ * @param {string} errorMessage
+ * @param {TextBox[]} textBoxes
+ * @param {string[]} correctFormattedValues
+ * @returns {Promise<null>}
+ */
+const displayError = (
+  errorTitle,
+  errorMessage,
+  textBoxes,
+  correctFormattedValues,
+) => {
+  return new Promise((resolve) => {
+    const msgBox = document.createElement("div");
+
+    const elmTitle = document.createElement("div");
+    elmTitle.textContent = errorTitle;
+
+    const elmWhatProblem = document.createElement("div");
+    elmWhatProblem.textContent = errorMessage;
+    elmWhatProblem.className = "mt-4 mb-6";
+
+    const button = document.createElement("input");
+    button.type = "button";
+    button.value = "閉じる";
+    button.className =
+      "w-24 cursor-pointer rounded-md border border-sky-500 bg-gray-200 px-4 hover:bg-gray-300 focus:ring-2 focus:ring-sky-700 focus:outline-none text-center";
+
+    // utl.displayMessageInFullScreen.open の引数を msgBox のみに修正する！！！！！！！！！！！
+    utl.displayMessageInFullScreen.open(msgBox);
+  });
+};
+
 //////////////////////////////////////////////////////////////////////
 //  class TextBox
 //////////////////////////////////////////////////////////////////////
@@ -15,30 +68,12 @@ export class TextBox {
   /** @type {string} */
   #lastFormattedValue;
 
-  /**
-   * @callback CallbackFormat
-   * @param {string} validValue
-   * @returns {string} formattedValue
-   */
   /** @type {CallbackFormat[]} */
   #callbackFormats = [];
 
-  /**
-   * @callback CallbackCheck
-   * @param {string} value
-   * @param {string} lastFormattedValue
-   * @returns {{validValue: string} | {errorMessage: string, correctFormattedValue: string}} result
-   */
   /** @type {CallbackCheck[]} */
   #callbackChecks = [];
 
-  /**
-   * @callback CallbackMultipleCheck
-   * @param {string} senderValue
-   * @param {string} senderLastFormattedValue
-   * @param {TextBox} SenderTextBox
-   * @returns {Promise<{validValue: string} | {errorMessage: string, textBoxes: TextBox[], correctFormattedValues: string[]}>} result
-   */
   /** @type {CallbackMultipleCheck[]} */
   #callbackMultipleChecks = [];
 
@@ -112,6 +147,7 @@ export class TextBox {
   }
 
   /**
+   * エラーメッセージを表示して、値を修正する
    *
    * @returns {Promise<{string | null}>}
    */
@@ -121,6 +157,13 @@ export class TextBox {
       this.formatToInput(result.validValue);
       return this.checkFromInput().validValue;
     }
+
+    await displayError(
+      "「xx」で入力エラーが発生しました",
+      result.errorMessage,
+      result.textBoxes,
+      result.correctFormattedValues,
+    );
   };
 
   /**
@@ -133,8 +176,8 @@ export class TextBox {
     this.#input = input;
     this.#label = label;
     this.formatToInput(validValue);
-    this.#input.addEventListener("click", () => {
-      //
+    this.#input.addEventListener("click", async () => {
+      await this.correct();
     });
   }
 }
